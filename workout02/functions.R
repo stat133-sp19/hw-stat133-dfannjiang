a621701118cc578f59e57bfc8f71c6dccd8fba66
Dominic Fannjiang <dfannjiang@gmail.com>
2019-04-09 09:54:42 -0700

    workout2: final version

--- workout02/functions.R
+++ workout02/functions.R
@@ -3,15 +3,15 @@ future_value <- function(amount, rate, years) {
 }
 
 annuity <- function(contrib, rate, years) {
-  if (rate == 0) {
-    return(contrib*years)
-  }
+  # if (rate == 0) {
+  #   return(contrib*years)
+  # }
   return(contrib*((1 + rate)^years - 1) / rate)
 }
 
 growing_annuity <- function(contrib, rate, growth, years) {
-  if (rate == growth) {
-    return(contrib * (1 + years) / (1 + rate))
-  }
+  # if (rate == growth) {
+  #   return(contrib * years / (1 + rate))
+  # }
   return(contrib*((1 + rate)^years - (1 + growth)^years) / (rate - growth))
 }
